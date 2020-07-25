c190574ec19a8c8832c55b6f687999eb93bc6302
Taylor Chen <s68175@gmail.com>
2020-07-26 02:51:55 +0800

    Add modify restaurant

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const Handlebars = require("handlebars")
 
 const app = express()
 
 const port = 3000
+
+// register helper
+Handlebars.registerHelper("categorySame", function (category, value, options) {
+  if (category === value) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+})
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
@@ -70,17 +80,43 @@ app.get('/add', (req, res) => {
   return res.render('add')
 })
 
-// add new restaurant
 app.post('/add_restaurant', (req, res) => {
   const restaurant = req.body
 
   if ((restaurant.name.length === 0) || (restaurant.image.length === 0) ||
     (restaurant.location.length === 0) || (restaurant.phone.length === 0)) {
     return res.render('add', { restaurant })
-  }
-  else {
+  } else {
     return Restaurant.create(restaurant)
       .then(() => res.redirect('/'))
+      .catch(error => console.log(error))
+  }
+})
+
+// edit restaurant
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const restaurant_update = req.body
+
+  if ((restaurant_update.name.length === 0) || (restaurant_update.image.length === 0) ||
+    (restaurant_update.location.length === 0) || (restaurant_update.phone.length === 0)) {
+    return res.render('edit', { restaurant: restaurant_update })
+  } else {
+    return Restaurant.findById(id)
+      .then(restaurant => {
+        restaurant = Object.assign(restaurant, restaurant_update)
+        return restaurant.save()
+      })
+      .then(() => res.redirect(`/`))
       .catch(error => console.log(error))
   }
 })
